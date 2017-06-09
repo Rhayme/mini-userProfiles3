29a17312b32c82a205d413d42917a9fc1c927089
Ray Pena <rhayme00@gmail.com>
2017-06-08 18:33:27 -0600

    MiniPoreject done using

--- service.js
+++ service.js
@@ -1,10 +1,22 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    this.getUsers = function() {
+        var deferred = $q.defer(); //creates a promise
+        console.log(1111111, deferred);
+        $http({
+            method: 'GET',
+            url: 'https://reqres.in/api/users?page=1'
+        }).then(function(response) {
+
+            var users = response.data.data;
+
+            users.forEach(function(user) {
+                users.last_name = 'McIver';
+            });
+            deferred.resolve(users);
+
+        });
+        return deferred.promise;
+    }
 
 });
